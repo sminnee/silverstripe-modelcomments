8e5cf606a362e163851a384de899391aa16c900d
Sam Minnee <sam@silverstripe.com>
2020-07-01 10:43:04 +1200

    NEW: Initial MVP version of comments system.

---
+++ src/HasModelCommentsExtension.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Sminnee\ModelComments;
+
+use SilverStripe\ORM\DataObject;
+use SilverStripe\ORM\DataExtension;
+use SilverStripe\Forms\GridField AS GF;
+use Symbiote\GridFieldExtensions AS GFE;
+use Silverstripe\Forms AS F;
+
+class HasModelCommentsExtension extends DataExtension
+{
+    private static $has_many = [
+        'ModelComments' => ModelComment::class,
+    ];
+
+    public function updateCMSFields($fields)
+    {
+        $fields->fieldByName('Root')->fieldByName('ModelComments')->setTitle('Comments');
+
+        $fields->dataFieldByName('ModelComments')->setConfig((new GF\GridFieldConfig)
+            ->addComponent(new GF\GridFieldButtonRow('after'))
+            ->addComponent(new GFE\GridFieldTitleHeader())
+            ->addComponent((new GFE\GridFieldEditableColumns())
+                ->setDisplayFields([
+                    'Comment' => ['title' => 'Comment', 'callback' => function ($record, $column, $grid) {
+                        if ($record->ID) {
+                            return new F\ReadonlyField($column);
+                        } else {
+                            return new F\TextareaField($column);
+                        }
+                    }],
+
+                    'Author.Name' => ['title' => 'Author', 'field' => F\ReadonlyField::class],
+                    'CreatedString' => ['title' => 'Date', 'field' => F\ReadonlyField::class],
+                ])
+            )
+            // TO DO: amend this so that new records can be prepended rather than appended, and move to top of list
+            ->addComponent((new GFE\GridFieldAddNewInlineButton('buttons-after-left'))->setTitle('Post comment (press Save after commenting)'))
+            // TO DO: add pagination once the comments are list most-recent-first
+            //->addComponent(new GF\GridFieldPaginator(30))
+        );
+    }
+}
